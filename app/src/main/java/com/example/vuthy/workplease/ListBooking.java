bb8f14e8e5d411d79bb609f26cb158f41b5f5d57
Mengthong <mengthong.l@ligercambodia.org>
2017-11-22 22:24:52 +0700

    Upload booking driver code

--- app/src/main/java/com/example/vuthy/workplease/ListBooking.java
+++ app/src/main/java/com/example/vuthy/workplease/ListBooking.java
@@ -1,13 +1,31 @@
 package com.example.vuthy.workplease;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListBooking extends AppCompatActivity {
 
+    private Button chanceActivityButton;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_booking);
+
+        configureNextButton();
+    }
+    private void configureNextButton(){
+
+        Button nextButton = (Button) findViewById(R.id.bt1);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ListBooking.this,BookingInfo.class));
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_in_left);
+            }
+        });
     }
 }

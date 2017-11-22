3070308eec49203bfed90d789d41c27971cbdae3
Vuthy <vuthy.v@ligercambodia.org>
2017-11-22 22:05:26 +0700

    Add all pages
    Please add the files in here

---
+++ app/src/main/java/com/example/vuthy/workplease/BookingInfo.java
@@ -0,0 +1,13 @@
+package com.example.vuthy.workplease;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_info);
+    }
+}
